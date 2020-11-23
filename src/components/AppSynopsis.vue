f64b076cbfde0bb1582cc68ee72b220cbf850a68
Anderson Barros <anderson.barros@selcorp.com.br>
2020-11-23 20:52:06 -0300

    feat: Implements component to display synopsis

---
+++ src/components/AppSynopsis.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="synopsis-component">
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h3 class="title has-text-white">Sinopse</h3>
+          <div class="box custom-box-container">
+            <p class="synopsis-box">
+              {{ synopsis }}
+            </p>
+
+            <b-button type="is-dark" @click.stop.prevent="closeModal()">
+              Fechar
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSynopsis',
+  props: {
+    synopsis: { type: String, default: '' },
+  },
+  methods: {
+    closeModal() {
+      this.$parent.onCancel();
+      this.$parent.close();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.synopsis-box {
+  margin-bottom: 1rem;
+}
+</style>
