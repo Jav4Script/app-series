718cf237678e3d777ec6041f28fc493838c4dbae
Anderson Barros <anderson.barros@selcorp.com.br>
2020-11-23 20:52:35 -0300

    feat: Implements pagination component

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="is-flex is-justify-content-flex-end">
+    <b-field grouped>
+      <div>
+        <b-pagination
+          rounded
+          aria-next-label="Próxima Página"
+          aria-previous-label="Página anterior"
+          aria-page-label="Página"
+          aria-current-label="Página atual"
+          icon-prev="chevron-left"
+          icon-next="chevron-right"
+          order="is-right"
+          range-after="2"
+          range-before="2"
+          :current="page"
+          :total="count"
+          @change="changePage"
+        >
+        </b-pagination>
+      </div>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPagination',
+  props: {
+    count: { type: Number },
+    page: { type: Number },
+  },
+  methods: {
+    changePage(page) {
+      this.$emit('change-page', page);
+    },
+  },
+};
+</script>
